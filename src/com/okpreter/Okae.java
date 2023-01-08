5b437c02e6b661e0968dac385ed79230968f80ba
Russel Rai <leon.amorphy@googlemail.com>
2023-01-08 15:32:07 +0000

    create framework for the interpreter

--- src/com/okpreter/Okae.java
+++ src/com/okpreter/Okae.java
@@ -1,5 +1,52 @@
 package com.okpreter;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class Okae {
+
+  public static void main(String[] args) throws IOException {
+    if (args.length > 1) {
+      System.out.println("Usage: Okae [scripts]");
+      System.exit(64);
+    } else if (args.length == 1) {
+      runFile(args[0]);
+    } else {
+      rumPrompt();
+    }
+  }
+
+  private static void rumPrompt() throws IOException {
+    InputStreamReader input = new InputStreamReader(System.in);
+    BufferedReader reader = new BufferedReader(input);
+
+    for (; ; ) {
+      System.out.println("> ");
+      String line = reader.readLine();
+      if (line == null) {
+        break;
+      }
+      run(line);
+    }
+  }
+
+  private static void runFile(String path) throws IOException {
+    byte[] bytes = Files.readAllBytes(Paths.get(path));
+    run(new String(bytes, Charset.defaultCharset()));
+  }
+
+  private static void run(String source) {
+    Scanner scanner = new Scanner(source);
+    List<Token> tokens = scanner.scanTokens();
+
+    for (Token token : tokens) {
+      System.out.println(token);
+    }
+  }
 
 }
